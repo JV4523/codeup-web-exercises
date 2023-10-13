ce1999f6c03076a53182580a6d7ce686cc56f485
Joseph Vallas <jtv6@icloud.com>
2023-10-13 16:48:17 -0500

    Update

--- js/external.js
+++ js/external.js
@@ -22,15 +22,15 @@ alert("Your total is " + ((lm + bb + h) * p) + " dollars.")
 //Next Section
 let aRate = parseFloat(prompt("How much are you paid by Amazon per hour?"));
 let aHours = parseFloat(prompt ("How many hours did you work for Amazon this week?"));
-alert(`User worked for Amazon for ${aHours} hours this week at a rate of ${aRate.toFixed(2)} per hour. \n\n User is paid: ${(aHours * aRate).toFixed(2)}`);
+alert(`Employee worked for Amazon for ${aHours} hours this week at a rate of ${aRate.toFixed(2)} per hour. \n\n User is paid: ${(aHours * aRate).toFixed(2)}`);
 
 let mRate = parseFloat(prompt("How much are you paid by Meta per hour?"));
 let mHours = parseFloat(prompt ("How many hours did you work for Meta this week?"));
-alert(`User worked for Meta for ${mHours} hours this week at a rate of ${mRate.toFixed(2)} per hour. \n\n User is paid: ${(mHours * mRate).toFixed(2)}`);
+alert(`Employee worked for Meta for ${mHours} hours this week at a rate of ${mRate.toFixed(2)} per hour. \n\n User is paid: ${(mHours * mRate).toFixed(2)}`);
 
 let gRate = parseFloat(prompt("How much are you paid by Google per hour?"));
 let gHours = parseFloat(prompt ("How many hours did you work for Google this week?"));
 alert(`User worked for Google for ${gHours} hours this week at a rate of ${aRate.toFixed(2)} per hour. \n\n User is paid: ${(gHours * gRate).toFixed(2)}`);
 
 let fPay = (aRate * aHours) + (mRate * mHours) + (gRate * gHours);
-alert(`User's final take home pay is: ${fPay.toFixed(2)}`);
+alert(`Employee's final take home pay is: ${fPay.toFixed(2)}`);
