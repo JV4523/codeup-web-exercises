9edf4165ff45dbd1a4f93d56eec5b4dadd33d39d
Joseph Vallas <jtv6@icloud.com>
2023-10-13 12:09:53 -0500

    Upload Work

---
+++ js/external-js-example.js
@@ -0,0 +1,22 @@
+"use strict"//Makes Javascript more strict
+
+const pi = 3.14;
+console.log(pi);
+
+console.log(5+4);//Console logs print to the console and ONLY print to the console
+let instructorName = "David";
+console.log(instructorName);
+
+let favoriteColor = "Green";
+console.log(favoriteColor);
+//Alert Pop-up menu from user to Javascript
+alert("Howdy from Javascript");
+alert("howdy from " + instructorName);
+
+//Confirm  Pop-Up message from user to Javascript, that has the ability to capture a boolean
+let confirmed = confirm("We have lunch at 12:30 today?");
+console.log(confirmed);
+
+// Prompt - A pop-up message to the user from javascript that asks for a text view
+let userString = promote("what is your favorite baseball team");
+console.log()
